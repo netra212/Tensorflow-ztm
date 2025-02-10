3cea0f4cc310014c633daa59e613ee3a386550b1
netr212 <netra200021kcbdr@gmail.com>
2025-02-10 16:55:54 +0000

    Decorators in Python

---
+++ Fundamentals/Decorators.py
@@ -0,0 +1,33 @@
+'''
+Python decorator: allow us to modify or extend the behaviour of functions and methods without changing their actual code. When we use a a Python decorator, we wrap a function with another function, which takes the original function as an argument and returns its modified version. This techniques provides a siple way to implement higher-order functions in Python, enhanching code reusability and readability.
+
+'''
+
+# A function returns a value based on the given arguments.
+def add_one(number):
+    return number + 1
+
+# print(add_one(2))
+
+# Python treats function as first-class objects which means the function can be passed around and used as arguments just like any other object like str, int, float, list and so on. 
+# Example below:
+
+def say_hello(name):
+    return f"Hello {name}"
+
+def be_awesome(name):
+    return f"Yo {name}, together we're the awesomest!"
+
+# say_hello() and be_awesome() are regular functions that expect a name given as a string. 
+
+def greet_bob(greeter_func):
+    return greeter_func("Bob")
+
+# print(say_hello("Netra"))
+# print(be_awesome("Name"))
+print(greet_bob(say_hello)) # this say_hello function is named without parenthesis. This mean that only a reference to the function is passed. The function is not executed. The greet_bob() function, on the other hand, is written with parentheses, so it will be called as usual.
+print(greet_bob(be_awesome))
+
+'''
+Note: A function name without parenthesis is a reference to a function, while a function name with trailing parenthesis calls the function and refers to its return value. 
+'''
